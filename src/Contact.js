a62733500c7850ac14335bff80af7b4dc320c9ba
Hassan Fouad <mrhassanfouad1@gmail.com>
2023-01-01 17:36:55 +0200

    Social links

--- src/Contact.js
+++ src/Contact.js
@@ -9,10 +9,43 @@ import {
   Instagram,
   LinkedIn,
   Twitter,
+  YouTube,
 } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
+import axios from "axios";
+const socialLinksApi = `${process.env.NEXT_PUBLIC_API_URL}/public/social`;
 
+const getSocialLinksService = async () => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(socialLinksApi);
+    return {
+      data,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      error: error?.response?.data?.error?.message || "Unkown Error",
+    };
+  }
+};
 export default function Contact() {
+  const [socialLinks, setSocialLinks] = React.useState({});
+
+  const getSocialLinks = React.useCallback(async () => {
+    const { error, data } = await getSocialLinksService();
+    if (error) {
+      return;
+    } else {
+      setSocialLinks(data);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    getSocialLinks();
+  }, [getSocialLinks]);
+
   return (
     <Box width={1} sx={{ backgroundColor: "PRIMARY", py: 5 }} id="contact">
       <Container maxWidth="lg">
@@ -35,34 +68,61 @@ export default function Contact() {
           </Box>
 
           <Box display="flex">
-            <MuiLink href="email:app@email.com">
-              <IconButton sx={{ color: "white" }}>
-                <Email />
-              </IconButton>
-            </MuiLink>
+            {socialLinks.email && (
+              <MuiLink href={`email:${socialLinks.email}`}>
+                <IconButton sx={{ color: "white" }}>
+                  <Email />
+                </IconButton>
+              </MuiLink>
+            )}
 
-            <MuiLink href="phone:+201202021670">
-              <IconButton sx={{ color: "white" }}>
-                <Phone />
-              </IconButton>
-            </MuiLink>
-            <MuiLink href="https://facebook.com/foqsh" target="_blank">
-              <IconButton sx={{ color: "white" }}>
-                <Facebook />
-              </IconButton>
-            </MuiLink>
+            {socialLinks.phone && (
+              <MuiLink href={`tel:${socialLinks.phone}`}>
+                <IconButton sx={{ color: "white" }}>
+                  <Phone />
+                </IconButton>
+              </MuiLink>
+            )}
 
-            <MuiLink href="https://linkedin.com/in/hassanfouad" target="_blank">
-              <IconButton sx={{ color: "white" }}>
-                <LinkedIn />
-              </IconButton>
-            </MuiLink>
+            {socialLinks.facebook && (
+              <MuiLink href={socialLinks.facebook} target="_blank">
+                <IconButton sx={{ color: "white" }}>
+                  <Facebook />
+                </IconButton>
+              </MuiLink>
+            )}
 
-            <MuiLink href="https://facebook.com/foqsh" target="_blank">
-              <IconButton sx={{ color: "white" }}>
-                <Twitter />
-              </IconButton>
-            </MuiLink>
+            {socialLinks.linkedIn && (
+              <MuiLink href={socialLinks.linkedIn} target="_blank">
+                <IconButton sx={{ color: "white" }}>
+                  <LinkedIn />
+                </IconButton>
+              </MuiLink>
+            )}
+
+            {socialLinks.twitter && (
+              <MuiLink href={socialLinks.twitter} target="_blank">
+                <IconButton sx={{ color: "white" }}>
+                  <Twitter />
+                </IconButton>
+              </MuiLink>
+            )}
+
+            {socialLinks.youtube && (
+              <MuiLink href={socialLinks.youtube} target="_blank">
+                <IconButton sx={{ color: "white" }}>
+                  <YouTube />
+                </IconButton>
+              </MuiLink>
+            )}
+
+            {socialLinks.instagram && (
+              <MuiLink href={socialLinks.instagram} target="_blank">
+                <IconButton sx={{ color: "white" }}>
+                  <Instagram />
+                </IconButton>
+              </MuiLink>
+            )}
           </Box>
         </Box>
       </Container>
